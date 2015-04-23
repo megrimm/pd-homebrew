bb671f62c01f8ecb8b471cc55baeee2715133f4a
megrimm <megrimm@gmail.com>
2015-04-23 18:37:02 -0400

    add pd-gem

---
+++ Casks/pd-gem.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'pd-gem' do
+  version '0.93.3'
+  sha256 'fad5a77f01f25fb6b8c73e67d5d7ab3cc3ca87b5eb46d2fbe0be0e30d3f64823'
+
+  url "http://gem.iem.at/releases/0.93.3/Gem-0.93.3-OSX-ppc_i386.tar.gz"
+  name 'Gem'
+  homepage 'http://gem.iem.at/'
+  license :bsd
+
+  binary '' target ''
+
+end
