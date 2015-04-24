342b5f31055bcef7ccf669447653af81961ff69a
megrimm <megrimm@gmail.com>
2015-04-24 10:16:52 -0400

    replace 64-bit with 32-bit

--- Casks/pd.rb
+++ Casks/pd.rb
@@ -2,12 +2,12 @@ cask :v1 => 'pd' do
   version '0.46-6'
   sha256 'fad5a77f01f25fb6b8c73e67d5d7ab3cc3ca87b5eb46d2fbe0be0e30d3f64823'
 
-  url "http://msp.ucsd.edu/Software/pd-#{version}-64bit.mac.tar.gz"
+  url "http://msp.ucsd.edu/Software/pd-#{version}.mac.tar.gz"
   name 'Pd'
   homepage 'http://msp.ucsd.edu/software.html'
   license :bsd
 
-  app 'Pd-0.46-6-64bit.app'
+  app 'Pd-0.46-6.app', :target => 'Pd.app'
 
   postflight do
     system '/bin/chmod', '-R', '--', 'u+w', "#{staged_path}/Pd-0.46-5-64bit.app"
