ec8614a745a363e1e57a9f86edeaa4a45d846b0c
megrimm <megrimm@gmail.com>
2015-04-25 12:14:27 -0400

    add pd-gavl

---
+++ pd-gavl.rb
@@ -0,0 +1,34 @@
+require "formula"
+
+class Gavl < Formula
+  homepage "http://gmerlin.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/gmerlin/gavl/1.4.0/gavl-1.4.0.tar.gz"
+  sha1 "b1ef06942ff00b4d0c8dc361213a1542717855cc"
+
+  head 'svn://svn.code.sf.net/p/gmerlin/code/trunk/gavl', :revision => "4225"
+
+  head do
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'libtool' => :build
+    depends_on 'pkg-config' => :build
+    depends_on 'gettext' => :build
+    depends_on 'libsamplerate'
+  end
+
+  def install
+
+  if build.head?
+    inreplace 'autogen.sh', 'libtoolize', 'glibtoolize'
+    system "touch COPYING"
+  end
+  
+    system "./autogen.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--without-doxygen"
+    system "make","install"
+  end
+end
